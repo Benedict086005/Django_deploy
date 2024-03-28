d21b1a7e1b4584650540186ed0d7dcb883efc6ff
Benedict086005 <ajaniyeseer086005@gmail.com>
2024-03-29 00:51:04 +0100

    modify buil.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+if ! command -v pip &>/dev/null; then
+	echo "pip not found, installing pip..."
+	sudo apt update
+	sudo apt install python3-pip
+fi
+
 pip install -r requirements.txt
+
 python3.9 manage.py collectstatic
