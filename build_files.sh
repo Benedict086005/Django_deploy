161aa2e1d4c69c15bdc0d63c9791fcc2c25d1103
Benedict086005 <ajaniyeseer086005@gmail.com>
2024-03-29 08:12:04 +0100

    changes1

--- build_files.sh
+++ build_files.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Full path to pip executable
-PIP_EXEC=/usr/bin/pip
-
-# Check if pip is installed
-if ! command -v $PIP_EXEC &>/dev/null; then
-	echo "pip not found, installing pip..."
-	# Install pip
-	sudo apt update
-	sudo apt install python3-pip
-fi
-
-# Full path to python executable
-PYTHON_EXEC=/usr/bin/python3.9
-
-# Install dependencies from requirements.txt
-$PIP_EXEC install -r requirements.txt
-
-# Collect static files
-$PYTHON_EXEC manage.py collectstatic
+pip install -r requirements.txt
+python3.9 manage.py collectstatic
